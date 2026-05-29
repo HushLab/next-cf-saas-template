b8432e9f18b37dcd4fe4611367b5d64db641b8e4
hazeflame <myhazeflame@gmail.com>
2026-05-29 16:57:32 +0800

    Update client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,14 +1,17 @@
 import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { drizzle, type DrizzleD1Database } from "drizzle-orm/d1";
 
-import { createDb } from "@/db/create-db";
-import type { Database } from "@/db/create-db";
+import * as schema from "@/db/schema";
 
 type AppBindings = CloudflareEnv & {
 	DB: D1Database;
 };
 
-export { createDb };
-export type { Database };
+export type Database = DrizzleD1Database<typeof schema>;
+
+export function createDb(database: D1Database): Database {
+	return drizzle(database, { schema });
+}
 
 let cachedDb: Database | null = null;
 
@@ -16,6 +19,7 @@ export function getDb(): Database {
 	if (cachedDb) {
 		return cachedDb;
 	}
+
 	const { env } = getCloudflareContext();
 	cachedDb = createDb((env as AppBindings).DB);
 	return cachedDb;
